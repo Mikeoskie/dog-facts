fb4575c118dd7942c5c4ce1e677a0228cfd42604
Arif <arif.aydemir01@hotmail.com>
2024-02-21 22:51:46 +0300

    feature added show loading

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <p v-if="dogFact !== null"> {{ dogFact }}</p>
-    <p v-else>{{ errorMessage }}</p>
+    <p v-else-if="errorMessage">{{ errorMessage }}</p>
+    <p v-else>Loading...</p>
   </div>
 </template>
 
@@ -10,7 +11,7 @@ export default {
   data(){
     return {
       dogFact: null,
-      errorMessage: null
+      errorMessage: ''
     }
   },
   async created() {
@@ -19,13 +20,13 @@ export default {
       if(response.status === 200) {
         const data = await response.json()
         this.dogFact = data.facts[0]
-      } else if(response.status >= 400){
-        this.errorMessage = `Cannot load a dog fact: ${response.status} error`
-        console.log('API hatası', response)
+      } else {
+        this.errorMessage = `Sayfa yüklenirken bir hata oluştu: ${response.status} hata kodu`
+        console.log('API hatası', response.statusText)
       }
     }
     catch(error) {
-      this.errorMessage = error.message
+      this.errorMessage = `Sayfa yüklenirken bir hata oluştu: ${response.status} hata kodu`
       console.log('API hatası', error.message)
     }
   }
