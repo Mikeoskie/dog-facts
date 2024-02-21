79b76d565c3e8d30971f9da422b820eaa0747f5c
Arif <arif.aydemir01@hotmail.com>
2024-02-21 15:54:02 +0300

    API usage on the home page

--- app.vue
+++ app.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p> {{ dogFact }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      dogFact: ''
+    }
+  },
+  async created() {
+    try {
+      const response = await fetch('https://dog-api.kinduff.com/api/facts')
+      const data = await response.json()
+      if(response.status === 200) {
+        this.dogFact = data.facts[0]
+      } else {
+        console.log('API hatası', response.statusText)
+      }
+    }
+    catch(error) {
+      console.log('API hatası', error.message)
+    }
+  }
+}
+</script>
+
+ <style>
+ p {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+ }
+ </style>
